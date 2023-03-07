f28b77aedeea614ac09c8956cc26953fb0669854
bonemuffin <101133295+BoneMuffin@users.noreply.github.com>
2023-03-07 13:43:30 -0800

    update gameboard module

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-// DOM Elements
-const player1 = document.querySelector("#p1");
-const player2 = document.querySelector("#p2");
-const vsHuman = document.querySelector('#vsHuman');
-const vsAI = document.querySelector('#vsAI');
-const boxes = document.querySelectorAll('.box');
+// DOM Elements and manipulation
+const DomElements = (function() {
+    const player1 = document.querySelector("#p1");
+    const player2 = document.querySelector("#p2");
+    const vsHuman = document.querySelector('#vsHuman');
+    const vsAI = document.querySelector('#vsAI');
 
-const winningConditions = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-];
+    // Get all free spaces
+    const boxes = document.querySelectorAll('.box');
+
+    // Color current player
+    const showCurrentPlayer = (player) => {
+        if (player === 1) {
+            player1.classList.add("currentPlayer");
+            player2.classList.remove("currentPlayer");
+        } else if (player === 2) {
+            player2.classList.add("currentPlayer");
+            player1.classList.remove("currentPlayer");
+        }
+    }
+
+    const showGridSpaces = () => {
+        return spaces;
+    }
+
+    return { showGridSpaces, showCurrentPlayer }
+});
 
 // Game logic
 
@@ -17,20 +32,57 @@ const winningConditions = [
 const GameBoard =  (function() {
     // store the gameboard as an array inside of a Gameboard object
         let board = ["", "", "", "", "", "", "", "", ""];
-        
-        //board is inaccessible to other modules, but they can fetch it
+
+        const winningConditions = [
+            [0, 1, 2], 
+            [3, 4, 5], 
+            [6, 7, 8],
+            [0, 3, 6], 
+            [1, 4, 7], 
+            [2, 5, 8],
+            [0, 4, 8], 
+            [2, 4, 6]
+            ];
+
+        const elements = DomElements.showGridSpaces();
+
+            let availableMoves = 9;
+            let isPlayable = () => {
+                return availableMoves > 0 ? true : false;
+            }
+
+        const displayBoard = () => {
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].innerText = GameBoard[i];
+            }
+         };
+
+        // board is inaccessible to other modules, but can be fetched
+        const setBoard = (index, sign) => {
+            if (index > board.length) return;
+            board[index] = sign;
+         };
+
         const getBoard = () => {
-            return board;
-            }
+            if (index > board.length) return;
+                board[index] = sign;
+        };
 
-        const resetGameBoard = () => {
-             board = [["", "", "", "", "", "", "", "", ""]];
-            }
+        const resetBoard = () => {
+            board = ["", "", "", "", "", "", "", "", ""];
+            availableMoves = 9;
 
-        return {getBoard, resetGameBoard}
+            // Reset colors from previous game's winning move
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].classList.remove("winningMove");
+            }
+            displayBoard()
+        };
+               
+        return { displayBoard, setBoard, getBoard, resetBoard }
 })();
 
-// module for highlighting the player's turn, the winner player and the winning combo, reset board
+// module for highlighting the player's turn, the winner and the winning combo in the boxes
 let DisplayController = (function() {
     return {};
 })();
